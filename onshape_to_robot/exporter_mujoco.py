267f4e4f611b5f51eb0016b69a5ca57c5fb90c9f
JafarAbdi <jafar.uruc@gmail.com>
2026-01-18 14:29:16 +0000

    Sort mesh and material entries in mujoco exporter for deterministic output

--- onshape_to_robot/exporter_mujoco.py
+++ onshape_to_robot/exporter_mujoco.py
@@ -79,9 +79,9 @@ class ExporterMuJoCo(Exporter):
 
         # Asset (mesh & materials)
         self.append("<asset>")
-        for mesh_file in set(self.meshes):
+        for mesh_file in sorted(self.meshes):
             self.append(f'<mesh file="{mesh_file}" />')
-        for material_name, color in self.materials.items():
+        for material_name, color in sorted(self.materials.items()):
             color_str = "%g %g %g %g" % tuple(color)
             self.append(f'<material name="{material_name}" rgba="{color_str}" />')
         self.append("</asset>")
